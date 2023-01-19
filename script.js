19df30bba929c670d565df7005e0d782e9e926a7
YoungDemzi <sebastian.demzala@gmail.com>
2023-01-19 03:38:19 +0100

    quick fix

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const playRound = (playerSelection,computerSelection) =>{
      /* WIN CONDITION */
     else if((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||(playerSelection === 'PAPER' && computerSelection === 'ROCK')){
 
-        computerHealh[4-playerScore].style.color="black";
+        computerHealh[playerScore].remove();
         playerScore+=1;
         roundResult.textContent="YOU WON!";
         roundResult.style.color= "green";
@@ -60,7 +60,7 @@ const playRound = (playerSelection,computerSelection) =>{
     }
      /* LOSE CONDITION */
     else{
-        playerHealth[4-computerScore].style.color="black";
+        playerHealth[computerScore].remove();
         computerScore+=1;
         roundResult.textContent="YOU LOST!";
         roundResult.style.color= "brown";
